d156a1dad7b5879cb2a105fe09c321fbfa3ec715
Abhishek Prashant Chandurkar <cabhishek1304@gmail.com>
2025-01-30 17:04:07 +0530

    Final Evaluation Commit

--- Phase 1/Q7 Solution of Congruence.c
+++ Phase 1/Q7 Solution of Congruence.c
@@ -4,7 +4,7 @@
 
 void main(int argc,char *argv[])
 {
-	mpz_t a,x,b,m;
+	mpz_t a,x,b,m,m_org;
 	mpz_inits(a,x,b,m,NULL);
 
 	mpz_set_ui(a,atoi(argv[1]));
@@ -16,15 +16,32 @@ void main(int argc,char *argv[])
 
 	mpz_gcd(g,a,m);
 
-	if((mpz_divisible_p(b,g) == 0)||(mpz_cmp_ui(g,1)>0))
+	if(mpz_divisible_p(b,g) == 0)
 	{
 		printf("%ld",mpz_get_ui(g));
 		return;
 	}
 	else
 	{
-		// x is MI of a
+		// Reduce the eqn by dividing by g on both sides
+
+		//printf(" %ld ",mpz_get_ui(g));
+		mpz_fdiv_q(a,a,g);
+		mpz_fdiv_q(b,b,g);
+
+		mpz_init_set(m_org,m);
+		mpz_fdiv_q(m,m,g);
+
+		// Multiply both sides by a^-1
+
 		mpz_invert(x,a,m);
+	
+		mpz_mul(b,b,x);
+		mpz_mod(b,b,m);
+
+		//printf(" %ld ",mpz_get_ui(b));
+		mpz_set(x,b);
+
 		mpz_t i;
 		mpz_init_set_ui(i,0);
 		
@@ -34,15 +51,16 @@ void main(int argc,char *argv[])
 		mpz_t m_by_g;
 		mpz_init(m_by_g);
 
-		mpz_fdiv_q(m_by_g,m,g);
+		mpz_fdiv_q(m_by_g,m_org,g);
 
-		while(mpz_cmp(i,g)<=0)
+		while(mpz_cmp(i,g)<0)
 		{
-			printf("%ld ",mpz_get_ui(soln));
+			printf("x = %ld \n",mpz_get_ui(soln));
 
 			mpz_add(soln,soln,m_by_g);	
 			mpz_add_ui(i,i,1);
 		}
+		printf("Number of solutions : %ld \n",mpz_get_ui(g));
 	}
 }
 
